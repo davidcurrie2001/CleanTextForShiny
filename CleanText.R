ddef835cbb76ba52afa13ddd756ae43201e31aeb
David Currie <davidcurrie2001@yahoo.co.uk>
2019-11-18 17:30:37 +0000

    Show words with non ascii characters

--- CleanText.R
+++ CleanText.R
@@ -1,37 +1,62 @@
 library(qdap)
 
-
 # Make sure the positions of the text you want to fidn and replace match up otherwise you'll produce nonsense
-toFind <- c("'","€","≤","≥","°", "–" )
-toReplace <- c("‘","&#8364 ", "&#8804 ", "&#8805 ", "&deg ", "-")
+toFind <- c("'","’","€","≤","≥","°", "–" )
+toReplace <- c("'","'","&#8364 ", "&#8804 ", "&#8805 ", "&deg ", "-")
 
 Fix <- data.frame(toFind, toReplace, stringsAsFactors=FALSE)
 
-# Load the data
-Introduction <- read.csv("Introduction.csv",stringsAsFactors = FALSE)
+filesToClean <- list.files(path = "ToClean/")
 
-myDF <- Introduction
-myDFClean <- Introduction
+for (myFile in filesToClean) {
 
-# There is probably a better way to do this using vectors...
-for (myCol in colnames(Introduction)){
+  # Load the data
+  #Introduction <- read.csv("ToClean/Introduction.csv",stringsAsFactors = FALSE)
+  myDF <- read.csv(paste("ToClean/",myFile, sep = ""),stringsAsFactors = FALSE)
   
-  #myCol <- colnames(Introduction)[[2]]
-  #myDF[,myCol]
+  #myDF <- Introduction
+  myDFClean <- myDF
   
-  # Do a find and replace using mgsub from the qdap package
-  myDFClean[,myCol] <- mgsub(Fix$toFind, Fix$toReplace, myDF[,myCol])
+  # There is probably a better way to do this using vectors...
+  for (myCol in colnames(myDF)){
     
+
+    print(paste("Processing",myCol,"in", myFile))
+    
+    # Do a find and replace using mgsub from the qdap package
+    myDFClean[,myCol] <- mgsub(Fix$toFind, Fix$toReplace, myDF[,myCol] )
+    
+    SplitText <- unlist(strsplit(as.character(myDF[,myCol]), split = " "))
+    SplitTextClean <- unlist(strsplit(as.character(myDFClean[,myCol]), split = " "))
+    
+    # Check the "before" file for non-ascii characters
+    myGResults <- grep("[^\\x00-\\x7F]", SplitText, perl=T)
+    if(length(myGResults)>0){
+      print("Now showing non-ascii characters before cleaning")
+      print(SplitText[myGResults])
+    } else {
+      #print("No non-ascii characters found before cleaning")
+    }
+    
+  
+    # Check the "before" file for non-ascii characters
+    myGResults <- grep("[^\\x00-\\x7F]", SplitTextClean, perl=T)
+    if(length(myGResults)>0){
+      print("Now showing remaining non-ascii characters after cleaning")
+      print(SplitTextClean[myGResults])
+    } else {
+      #print("No non-ascii characters found after cleaning")
+    }
+    
+
+  }
+  
+  # Save the cleaned files
+  write.csv(myDFClean,paste("Cleaned/", myFile, sep = ""))
+
 }
 
 
-write.csv(myDFClean,"Introduction_clean.csv")
 
-
-#SplitText <- unlist(strsplit(Introduction$X2018, split = " "))
-# https://stackoverflow.com/questions/37843545/how-to-determine-if-character-string-contains-non-roman-characters-in-r
-#SplitTextNonAscii <- grep("SplitText", iconv(SplitText, "latin1", "ASCII", sub="SplitText"))
-#library(tools)
-#showNonASCII(Introduction$X2018)
 
 
